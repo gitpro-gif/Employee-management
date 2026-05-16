f947e6b6d4005a9ec70c1ee607f77ae08e74c753
gitpro-gif <186488259+gitpro-gif@users.noreply.github.com>
2026-05-16 12:49:22 +0530

    Added post route

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = []
+
 @app.get("/")
 def checkHealth():
     return {
         "message" : "Fastapi is working before merge"
     }
 
+@app.post("/addstudent")
+def CreateEmployee(id: int, name: str, department: str):
+    student = {
+        "id": id,
+        "name": name,
+        "department": department
+    }
+    
+    students.append(student)
+    return {
+        "message" : "Student added successfully",
+        "data": student
+    }
 
