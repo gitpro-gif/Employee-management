658d8f17bcecca2c66ef1762b8b0fb9473beee01
gitpro-gif <186488259+gitpro-gif@users.noreply.github.com>
2026-05-16 12:42:40 +0530

    Added get route

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = []
+
 @app.get("/")
 def checkHealth():
     return {
         "message" : "Fastapi is working before merge"
     }
+    
+@app.get("/getallstudents")
+def getallStudents():
+    return {
+        "message" : students
+    }
+    
 
 
